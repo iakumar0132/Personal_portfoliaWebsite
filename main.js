6407441eb409ffcb8368b96a3878a3d60271e552
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2023-02-09 02:11:55 +0530

    added all files,first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log("Hello")
+console.log("Script running...")
+document.querySelector('.ham').addEventListener("click", ()=>{
+document.querySelector('.sidebar').classList.toggle('sidebarGo');
+})
